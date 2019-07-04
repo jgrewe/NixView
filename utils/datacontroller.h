0fee38237ea16a47e660e8ce0d33e1652c13be0f
Jan Grewe <jan.grewe@g-node.org>
2019-07-04 18:13:43 +0200

    [controller] implements a EntityInfo struct ...
    to be created by the controller and passed to the TreeModelItem

--- utils/datacontroller.h
+++ utils/datacontroller.h
@@ -3,6 +3,7 @@
 
 #include <nix.hpp>
 #include <QString>
+#include <QVariant>
 #include <boost/filesystem.hpp>
 #include "model/nixtreemodel.h"
 
@@ -57,6 +58,41 @@ struct FileInfo {
     }
 };
 
+struct EntityInfo {
+
+    QVariant created_at, updated_at, name, dtype, id, store_type, type, value;
+    NixType nix_type;
+
+    EntityInfo(nix::base::INamedEntity &item, const NixType &ntype) {
+        name = QVariant(item.name().c_str());
+        type = QVariant(item.type().c_str());
+        nix_type = ntype;
+        entity_props(item);
+        if (ntype == NixType::NIX_FEAT) {
+            nix::Property &p = dynamic_cast<nix::Property&>(item);
+            dtype = QVariant(nix::data_type_to_string(p.dataType()).c_str());
+        } else if (ntype == NixType::NIX_DATA_ARRAY) {
+            nix::DataArray &da = dynamic_cast<nix::DataArray&>(item);
+            dtype = QVariant(nix::data_type_to_string(da.dataType()).c_str());
+        } else {
+            dtype = QVariant("n.a.");
+        }
+    }
+
+    EntityInfo(nix::Feature &feat) {
+        name = QVariant(feat.data().name().c_str());
+        type = QVariant(feat.data().type().c_str());
+        nix_type = NixType::NIX_FEAT;
+        dtype = QVariant(nix::data_type_to_string(feat.data().dataType()).c_str());
+        entity_props(dynamic_cast<nix::base::IEntity&>(feat));
+    }
+
+    void entity_props(const nix::base::IEntity &ent) {
+        id = QVariant(ent.id().c_str());
+        created_at = QVariant(nix::util::timeToStr(ent.createdAt()).c_str());
+        updated_at = QVariant(nix::util::timeToStr(ent.updatedAt()).c_str());
+    }
+};
 
 class DataController
 {
